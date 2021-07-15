d752d1af1d88c5dd4b82141b4f8b60014e2e7b35
takurinton <takurinton@takurinton.com>
2021-07-16 00:13:31 +0900

    delete: callback

--- src/getTweet.js
+++ src/getTweet.js
@@ -1,14 +1,6 @@
 const { client, SCREEN_NAME } = require('./init');
 
 const params = { screen_name: SCREEN_NAME };
-
-const callBackTweet = (err, tweets, _, text) => {
-  if (!err) {
-    tweets.map(tweet => text+=tweet.text);
-  };
-  return text;
-};
-
 const getTweet = async () => {
   const res = await client.get('statuses/user_timeline', params);
   const text = res.map(tweet => tweet.text).join('')
