0b267e8b16dd075bf6c441cdb5d43dbe34253a44
takurinton <takurinton@takurinton.com>
2021-07-21 23:09:17 +0900

    fix

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ const tweet = async () => {
     const sentence = await getTweet();
     text = createSentence(sentence);
     console.log(`text content: ${text}`);
-    console.log(`text length: ${text.length}, recharenge!!!`);
-    console.log(``);
+    console.log(`text length: ${text.length}`);
     if (text.length === 0 || text.length > 140) {
-        console.log('tweet length is 140 over. restart programm.')
+        console.log('tweet length is 140 over.');
+        console.log('restart programm.');
+        console.log('');
         tweet();
     } else {
         postTweet(text);
